10b2fdea90eb0f1c9c18af5ab0122b79a4321058
jstuart <jstuart@redhat.com>
2018-11-26 14:37:04 -0600

    removing map

--- src/jobs/pipelineJobs.groovy
+++ src/jobs/pipelineJobs.groovy
@@ -2,6 +2,6 @@ import org.centos.contra.jobdsl.PipelineJob
 
 
 def job = new PipelineJob(this, 'metricsPipelineJob')
-job.addGit([branch: 'master', repoUrl: 'https://gitlab.sat.engineering.redhat.com/cpaas/pipeline.git'])
+job.addGit(branch: 'master', repoUrl: 'https://gitlab.sat.engineering.redhat.com/cpaas/pipeline.git')
 job.gitlabTrigger(build_on_merge_request_events: true, build_on_push_events: true)
 job.logRotate()
