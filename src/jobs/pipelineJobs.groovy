80370941c695a735fcfa9fd20a70af9f1dcc0c5f
jstuart <jstuart@redhat.com>
2019-01-14 15:04:26 -0600

    updating demo jobs

--- src/jobs/pipelineJobs.groovy
+++ src/jobs/pipelineJobs.groovy
@@ -2,7 +2,7 @@ import org.centos.contra.jobdsl.PipelineJob
 
 
 def job = new PipelineJob(this, 'metricsPipelineJob')
-job.addGit(branch: 'master', repoUrl: 'https://github.com/joejstuart/pipeline.git')
+job.addGit(repo_branch: 'master', repo_url: 'https://github.com/joejstuart/pipeline.git')
 job.gitlabTrigger(build_on_merge_request_events: true, build_on_push_events: true)
 job.logRotate()
 
